8f287032a6aab63b6589ed2b32347272366fcdea
Verdegem Tom <tom.verdegem@student.howest.be>
2024-02-07 20:12:59 +0100

    add code to branch off in result and lab

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+"use strict"
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Hello World
+    const helloWorldReqButton = document.querySelector("#hello-world-button");
+    const helloWorldClear = document.querySelector("#hello-world-clear");
+    const helloWorldOutput = document.querySelector("#hello-world-output");
+
+    helloWorldReqButton.addEventListener('click', sendHelloWorldRequest);
+    helloWorldClear.addEventListener('click', function() {
+        clearOutput(helloWorldOutput);
+    });
+
+    // Random Number
+    const randomNumberReqButton = document.querySelector("#random-number-button");
+    const randomNumberClear = document.querySelector("#random-number-clear");
+    const randomNumberOutput = document.querySelector("#random-number-output");
+
+    randomNumberReqButton.addEventListener('click', sendRandomNumberRequest);
+    randomNumberClear.addEventListener('click', function() {
+        clearOutput(randomNumberOutput);
+    });
+
+    // Char Length
+    const charLengthReqButton = document.querySelector("#char-length-button");
+    const charLengthClear = document.querySelector("#char-length-clear");
+    const charLengthOutput = document.querySelector("#char-length-output");
+
+    charLengthReqButton.addEventListener('click', sendCharLengthRequest);
+    charLengthClear.addEventListener('click', function() {
+        clearOutput(charLengthOutput);
+    });
+});
+
+function sendHelloWorldRequest() {
+    const outputElement = document.querySelector('#hello-world-output');
+    outputElement.textContent = 'No HTTP request made yet';
+}
+
+function sendRandomNumberRequest() {
+    const outputElement = document.querySelector('#random-number-output');
+    outputElement.textContent = 'No HTTP request made yet';
+}
+
+function sendCharLengthRequest() {
+    const outputElement = document.querySelector('#char-length-output');
+    outputElement.textContent = 'No HTTP request made yet';
+}
+
+function clearOutput(selector) {
+    selector.textContent ="";
+}
